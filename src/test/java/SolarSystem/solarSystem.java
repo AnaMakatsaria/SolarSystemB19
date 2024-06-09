e08465dd4f743e691abc40b773af84cdd233f1d2
AnaMakatsaria <anushki1@gmail.com>
2024-06-09 17:55:48 -0400

    done with planet1

--- src/test/java/SolarSystem/solarSystem.java
+++ src/test/java/SolarSystem/solarSystem.java
@@ -14,5 +14,11 @@ public class solarSystem {
         xros.temp="24324kv";
         xros.size="0km";
 
+        //..
+        planet1 renox= new planet1();
+        renox.color="grey";
+        renox.size="10km";
+        renox.radius="5m";
+
     }
 }
